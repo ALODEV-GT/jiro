838e4f56c39e71769e5896855611a3ead478c358
Alex Alonzo <brayan.quialo@gmail.com>
2025-12-10 18:54:02 -0600

    feat(app): Add api config service

---
+++ src/app/shared/services/api-config.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiConfig {
+    private readonly API_BASE = environment.API_BASE;
+
+    public readonly API_AUTH = `${this.API_BASE}/auth`
+
+}
